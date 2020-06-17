c81656711bccf4b49b7eed160694aa22c94f2d9f
Jacob B. Madsen <jacob@jbmadsen.com>
2020-06-17 21:38:52 +0200

    Scoping out SQL helper code for Redshift staging tables

--- airflow/plugins/helpers/__init__.py
+++ airflow/plugins/helpers/__init__.py
@@ -1,2 +1,5 @@
+from helpers.redshift_sql_queries import SqlQueries
+
 __all__ = [
+    'SqlQueries',
 ]
---
+++ airflow/plugins/helpers/redshift_sql_queries.py
@@ -0,0 +1,17 @@
+class SqlQueries:
+    create_staging_tables = """
+        DROP TABLE IF EXISTS public.staging_metadata;
+        CREATE TABLE IF NOT EXISTS public.staging_metadata (
+            id varchar(256),
+            submitter varchar(256),
+            authors varchar(256),
+            title varchar(256),
+            comments varchar(256),
+            journalref varchar(256),
+            doi varchar(256),
+            abstract varchar(4000),
+            reportno varchar(256),
+            categories varchar(256),
+            versions varchar(256),
+        );
+    """
