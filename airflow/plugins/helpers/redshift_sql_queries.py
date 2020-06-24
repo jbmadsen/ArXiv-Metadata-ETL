633a236c2f41fa69cc44bd5de3869b004643564d
Jacob B. Madsen <jacob@jbmadsen.com>
2020-06-24 23:21:40 +0200

    Re-designed some table names, and are working on transformation queries

--- airflow/plugins/helpers/redshift_sql_queries.py
+++ airflow/plugins/helpers/redshift_sql_queries.py
@@ -40,28 +40,30 @@ class RedshiftSqlQueries:
     create_main_tables = """
         DROP TABLE IF EXISTS public.articles_fact;
         CREATE TABLE IF NOT EXISTS public.articles_fact (
-            article_id INT PRIMARY KEY,
+            id INT IDENTITY(0,1),
+            article_id VARCHAR(256),
+            year VARCHAR(20),
             submitter_name VARCHAR(256),
             title VARCHAR(256),
             comments VARCHAR(256),
-            journalref VARCHAR(256),
+            journal_reference VARCHAR(256),
             doi VARCHAR(256),
             abstract VARCHAR(256),
-            reportno VARCHAR(256),
+            report_number VARCHAR(256),
             classifications VARCHAR(256),
             versions VARCHAR(256)
         );
 
         DROP TABLE IF EXISTS public.versions_dim;
         CREATE TABLE IF NOT EXISTS public.versions_dim (
-            id INT PRIMARY KEY,
+            id INT IDENTITY(0,1),
             article_id INT,
             author VARCHAR(256)
         );
 
         DROP TABLE IF EXISTS public.authors_dim;
         CREATE TABLE IF NOT EXISTS public.authors_dim (
-            id INT PRIMARY KEY,
+            id INT IDENTITY(0,1),
             article_id INT,
             metadata_id VARCHAR(256),
             author_name VARCHAR(256)
@@ -69,7 +71,7 @@ class RedshiftSqlQueries:
 
         DROP TABLE IF EXISTS public.classifications_dim;
         CREATE TABLE IF NOT EXISTS public.classifications_dim (
-            id INT PRIMARY KEY,
+            id INT IDENTITY(0,1),
             article_id INT,
             tag_id VARCHAR(256),
             tag_name VARCHAR(256)
@@ -77,7 +79,7 @@ class RedshiftSqlQueries:
 
         DROP TABLE IF EXISTS public.citations_dim;
         CREATE TABLE IF NOT EXISTS public.citations_dim (
-            id INT PRIMARY KEY,
+            id INT IDENTITY(0,1),
             article_from_id INT,
             article_to_id INT,
             citation_from VARCHAR(256),
@@ -86,26 +88,18 @@ class RedshiftSqlQueries:
     """
 
     insert_articles_fact = """
-        INSERT INTO public.articles_fact
+        INSERT INTO public.articles_fact (article_id, submitter_name, year, title, comments, journal_reference, doi, abstract, report_number, classifications, versions)
         SELECT DISTINCT
-            md5(events.sessionid || events.start_time) songplay_id,
-            events.start_time, 
-            events.userid, 
-            events.level, 
-            songs.song_id, 
-            songs.artist_id, 
-            events.sessionid, 
-            events.location, 
-            events.useragent
-        FROM (
-            SELECT 
-                TIMESTAMP 'epoch' + ts/1000 * interval '1 second' AS start_time, 
-                *
-            FROM staging_events
-            WHERE page='NextSong'
-        ) events
-        LEFT JOIN staging_songs songs
-            ON events.song = songs.title
-                AND events.artist = songs.artist_name
-                AND events.length = songs.duration;
+            id as article_id,
+            submitter as submitter_name,
+            regexp_substr("journal-ref",'(19[6-9][0-9])|(20[0-9]{2})') as year,
+            title,
+            comments,
+            "journal-ref" as journal_reference,
+            doi,
+            abstract,
+            "report-no" as report_number,
+            categories as classifications,
+            versions
+        FROM staging.metadata
     """
